1c660fdc94c2b1310d7ade124488819fce8a15f9
Marcus Gustafsson <margu478@student.liu.se>
2016-01-22 13:45:43 +0100

    Some refactoring done, fixed problem with syntax highlightning

--- decryptPB.js
+++ decryptPB.js
@@ -2,18 +2,15 @@ var paste_enc_pref = "//+paste-encr";
 
 var new_line_start = "<li class='li1'><div class='de1'>";
 var new_line_end = "</div></li>";
+var text_patt = new RegExp("/archive/text");
 
-function htmlEscape(str) {
-    return String(str)
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-}
+function createPasteText(text, html) {
+    if (text_patt.exec(html) == null) {
+        return new_line_start + hljs.highlightAuto(text).value + new_line_end;
+    } else {
+        return new_line_start + text + new_line_end;
+    }
 
-function createPasteText(text) {
-    return new_line_start + hljs.highlightAuto(text).value + new_line_end;
 }
 
 function decryptData(paste_text, password) {
@@ -28,48 +25,51 @@ function decryptData(paste_text, password) {
     return dec_data;
 }
 
+function decryptPaste(paste_text, paste_id, callback) {
+    if (paste_text.slice(0, paste_enc_pref.length) == paste_enc_pref) {
+        pastePwExists(paste_id, function(exists){
+           if (exists) {
+               alertify.prompt("Paste ID found in DB, input your master password!", function(master_pw, ev){
+                  getPastePw(paste_id, master_pw, function(password) {
+                      var dec_data = decryptData(paste_text, password);
+                      callback(dec_data);
+                  })
+               });
+           } else {
+               alertify.prompt("This paste is encrypted.\nPlease input a password!", function (password, ev) {
+                   try {
+                       var dec_data = decryptData(paste_text, password);
+                       alertify.prompt("Save this password with your master password", function (master_pw, ev) {
+                           savePastePw(paste_id, password, master_pw);
+                       });
+                   } catch (e) {
+                       alertify.error("Wrong password!");
+                   }
+                   callback(dec_data);
+               });
+           }
+        });
+    }
+}
+
+
 function decryptIndex(paste_text, paste_id) {
     //Non-raw pastebin
+    var html = $("html").html();
     if (paste_text.slice(0, paste_enc_pref.length) == paste_enc_pref) {
         var paste_ol = $("ol");
         var raw_paste = $("#paste_code");
 
-        paste_ol.empty().append(createPasteText("Encrypted!"));
+        paste_ol.empty().append(createPasteText("Encrypted!", html));
         raw_paste.empty().val("Encrypted!");
 
-        pastePwExists(paste_id, function(exists) {
-            if (exists) {
-                alertify.prompt("Paste ID found in DB, input your master password!", function(master_pw, ev) {
-                    getPastePw(paste_id, master_pw, function(password) {
-                        var dec_data = decryptData(paste_text, password);
-                        raw_paste.empty().val(dec_data);
+        decryptPaste(paste_text, paste_id, function(dec_data) {
+            raw_paste.empty().val(dec_data);
 
-                        var lines = dec_data.split('\n');
-                        paste_ol.empty();
-                        for (var i = 0; i < lines.length; i++) {
-                            paste_ol.append(createPasteText(lines[i]));
-                        }
-                    });
-                });
-            } else {
-                alertify.prompt("This paste is encrypted.\nPlease input a password!", function(password, ev) {
-                    try {
-                        var dec_data = decryptData(paste_text, password);
-                        alertify.prompt("Save this password with your master password", function(master_pw, ev) {
-                            savePastePw(paste_id, password, master_pw);
-                        });
-                    } catch(e) {
-                        alertify.error("Wrong password!");
-                        return;
-                    }
-                    raw_paste.empty().val(dec_data);
-
-                    var lines = dec_data.split('\n');
-                    paste_ol.empty();
-                    for (var i = 0; i < lines.length; i++) {
-                        paste_ol.append(createPasteText(lines[i]));
-                    }
-                });
+            var lines = dec_data.split('\n');
+            paste_ol.empty();
+            for (var i = 0; i < lines.length; i++) {
+                paste_ol.append(createPasteText(lines[i], html));
             }
         });
     }
@@ -78,32 +78,11 @@ function decryptIndex(paste_text, paste_id) {
 function decryptRaw(paste_text, paste_id) {
     //Decrypts /raw
     if (paste_text.slice(0, paste_enc_pref.length) == paste_enc_pref) {
-
         var html_selector = $("body pre");
         html_selector.html("Encrypted!");
-        pastePwExists(paste_id, function(exists) {
-            if (exists) {
-                alertify.prompt("Paste ID found in DB, input your master password!", function(master_pw, ev) {
-                   getPastePw(paste_id, master_pw, function(password) {
-                       var dec_data = decryptData(paste_text, password);
-                       html_selector.html(dec_data);
-                   });
-                });
-            } else {
-                alertify.prompt("This paste is encrypted.\nPlease input a password!", function(password, ev) {
-                    try {
-                        var dec_data = decryptData(paste_text, password);
-                        alertify.prompt("Save this password with your master password", function(master_pw, ev) {
-                            savePastePw(paste_id, password, master_pw);
-                        });
-                    } catch(e) {
-                        alertify.error("Wrong password!");
-                        return;
-                    }
-                    html_selector.html(dec_data);
-                });
-            }
-        })
+        decryptPaste(paste_text, paste_id, function(dec_data) {
+            html_selector.html(dec_data);
+        });
     }
 }
 
