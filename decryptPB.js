bbb53477d7d5ad91da7c768107a30acaad988fbf
Marcus Gustafsson <margu478@student.liu.se>
2016-01-24 21:43:10 +0100

    Removed use of master password

--- decryptPB.js
+++ decryptPB.js
@@ -25,35 +25,21 @@ function decryptData(paste_text, password) {
     return dec_data;
 }
 
-function decryptPaste(paste_text, paste_id, callback) {
+function decryptPaste(paste_text, callback) {
     if (paste_text.slice(0, paste_enc_pref.length) == paste_enc_pref) {
-        pastePwExists(paste_id, function(exists){
-           if (exists) {
-               alertify.prompt("Paste ID found in DB, input your master password!", function(master_pw, ev){
-                  getPastePw(paste_id, master_pw, function(password) {
-                      var dec_data = decryptData(paste_text, password);
-                      callback(dec_data);
-                  })
-               });
-           } else {
-               alertify.prompt("This paste is encrypted.\nPlease input a password!", function (password, ev) {
-                   try {
-                       var dec_data = decryptData(paste_text, password);
-                       alertify.prompt("Save this password with your master password", function (master_pw, ev) {
-                           savePastePw(paste_id, password, master_pw);
-                       });
-                   } catch (e) {
-                       alertify.error("Wrong password!");
-                   }
-                   callback(dec_data);
-               });
+        alertify.prompt("This paste is encrypted.\nPlease input a password!", function (password, ev) {
+           try {
+               var dec_data = decryptData(paste_text, password);
+           } catch (e) {
+               alertify.error("Wrong password!");
            }
+           callback(dec_data);
         });
     }
 }
 
 
-function decryptIndex(paste_text, paste_id) {
+function decryptIndex(paste_text) {
     //Non-raw pastebin
     var html = $("html").html();
     if (paste_text.slice(0, paste_enc_pref.length) == paste_enc_pref) {
@@ -63,7 +49,7 @@ function decryptIndex(paste_text, paste_id) {
         paste_ol.empty().append(createPasteText("Encrypted!", html));
         raw_paste.empty().val("Encrypted!");
 
-        decryptPaste(paste_text, paste_id, function(dec_data) {
+        decryptPaste(paste_text, function(dec_data) {
             raw_paste.empty().val(dec_data);
 
             var lines = dec_data.split('\n');
@@ -75,35 +61,34 @@ function decryptIndex(paste_text, paste_id) {
     }
 }
 
-function decryptRaw(paste_text, paste_id) {
+function decryptRaw(paste_text) {
     //Decrypts /raw
     if (paste_text.slice(0, paste_enc_pref.length) == paste_enc_pref) {
         var html_selector = $("body pre");
         html_selector.html("Encrypted!");
-        decryptPaste(paste_text, paste_id, function(dec_data) {
+        decryptPaste(paste_text, function(dec_data) {
             html_selector.html(dec_data);
         });
     }
 }
 
-function decryptClone(paste_text, paste_id) {
+function decryptClone(paste_text) {
     if (paste_text.slice(0, paste_enc_pref.length) == paste_enc_pref) {
         var html_selector = $("#paste_code");
         html_selector.val("Encrypted!");
 
-        decryptPaste(paste_text, paste_id, function(dec_data) {
+        decryptPaste(paste_text, function(dec_data) {
            html_selector.val(dec_data);
         });
     }
 }
 
-var paste_id = window.location.pathname.split("/");
 
 if(window.location.pathname.slice(0,5) === "/raw/") {
-    decryptRaw($("body pre").text(), paste_id[2]);
+    decryptRaw($("body pre").text());
 } else if (window.location.pathname.slice(0,"/index/".length) === "/index/") {
-    decryptClone($("#paste_code").val(), paste_id[2]);
+    decryptClone($("#paste_code").val());
 } else {
     var paste_text = $("#selectable").text().slice(4).trim();
-    decryptIndex(paste_text, paste_id[1]);
+    decryptIndex(paste_text);
 }
